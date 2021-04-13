eeb6d915a8f85e12d46d7175c62c8a62c6aeaef1
rokadam <roshan.kadam@here.com>
2021-04-13 12:14:16 +0530

    SECURO-1 add health bean defination with scope singleton

--- src/main/java/org/hachiko/service/securo/resource/HomeResource.java
+++ src/main/java/org/hachiko/service/securo/resource/HomeResource.java
@@ -1,6 +1,7 @@
 package org.hachiko.service.securo.resource;
 
 import org.hachiko.service.securo.model.Health;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.log4j.Logger;
 
@@ -8,9 +9,12 @@ import org.apache.log4j.Logger;
 public class HomeResource implements HomeResourceInterface {
     static Logger logger = Logger.getLogger(HomeResource.class.getName());
 
+    @Autowired
+    Health health;
+
     @Override
     public Health health() {
         logger.info(String.format("%s", "Calling health resource"));
-        return new Health("OK");
+        return health;
     }
 }
